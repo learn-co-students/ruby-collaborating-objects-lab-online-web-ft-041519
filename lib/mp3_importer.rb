f7c55b53ba4d00c0dcaba90104eca4ac2d691f3a
Erin Stalzer <erinstalzer@gmail.com>
2019-05-06 14:42:53 +0000

    Done.

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -2,19 +2,21 @@ require "pry"
 
 class MP3Importer
 
-  attr_accessor :file,
+  @@filepathDirectory = []
 
-  def initialize(file)
-    @file = file
+  attr_accessor :filepath,
 
+  def initialize(filepath)
+    @filepath = filepath
+    @@filepathDirectory << filepath
   end
 
   def files
-
+    @@filepathDirectory
   end
 
-  def import
-
+  def import(filename)
+    self.new_by_filename(filename)
   end
 
 end
